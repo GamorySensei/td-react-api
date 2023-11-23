48c1dc4f57e6eaa617df3de2278e972ab5c95640
GamorySensei <theo.gamory@metznumericschool.fr>
2023-11-23 17:00:27 +0100

    Ajout d'un sélécteur de limite pour la pagination

--- src/components/Employee/EmployeeList.js
+++ src/components/Employee/EmployeeList.js
@@ -5,20 +5,21 @@ function EmployeeList() {
     // On crée une variable d'état pour stocker la liste des employés pour pouvoir agir dynamiquement dessus
     const [employees, setEmployees] = useState([]);
     const [pagesCount, setPagesCount] = useState();
+    const [pageLimit, setPageLimit] = useState(5);
 
     // On recupère la liste des employés via l'API au "montage" du composant
     useEffect(() => {
-        fetch('https://dummyjson.com/users') // Renvoie une Promesse
+        fetch('https://dummyjson.com/users?limit=' + pageLimit) // Renvoie une Promesse
         .then(response => response.json()) // On parse la réponse en Json
         .then(data => { // On traîte les données
             setEmployees(data.users);
             setPagesCount(Math.ceil(data.total/data.limit));
         });
-    }, []);
+    }, [pageLimit]);
 
     const handlePageChange = (page) =>  {
-        let skip = (parseInt(page) * 30)-30;
-        fetch('https://dummyjson.com/users?skip=' + skip) // Renvoie une Promesse
+        let skip = (parseInt(page) * pageLimit)-pageLimit;
+        fetch('https://dummyjson.com/users?skip=' + skip + '&limit=' + pageLimit) // Renvoie une Promesse
         .then(response => response.json()) // On parse la réponse en Json
         .then(data => {
             setEmployees(data.users);
@@ -27,20 +28,43 @@ function EmployeeList() {
 
     const renderPagination = () => {
         const pagination = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pagination.push(
-                <button key={i} onClick={ e => handlePageChange(i) }>
-                    {i}
-                </button>
-            );
+        if(pagesCount > 1){
+            for (let i = 1; i <= pagesCount; i++) {
+                pagination.push(
+                    <button key={i} onClick={ e => handlePageChange(i) }>
+                        {i}
+                    </button>
+                );
+            }
+            return pagination;
+        }else{
+            return null;
         }
-        return pagination;
     };
+
+    const handlePageLimitChange = (value) => {
+        setPageLimit(value);
+    }
+
+    const renderPageLimitSelector = () => {
+        let options = [5,10,20,30,50,100];
+        return (
+            <select onChange={(e) => handlePageLimitChange(e.target.value) }>
+                { options.map(limit => <option value={limit}>{limit}</option>) }
+            </select>
+        );
+    }
+
+    const renderEmployees = () => {
+        return employees.map(employee => <Employee key={ employee.id } data={employee} />) 
+    }
 
 
 
 	return <>
-        { employees.map(employee => <Employee key={ employee.id } data={employee} />) }
+        { renderPageLimitSelector() }
+
+        { renderEmployees() }
 
         { renderPagination() }
     </>;
