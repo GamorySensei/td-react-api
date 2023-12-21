ab341b98e564e02de18041acfd659df52ea2a436
GamorySensei <theo.gamory@metznumericschool.fr>
2023-12-21 10:48:33 +0100

    Mise en place du Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import './App.css';
 import EmployeeList from './components/Employee/EmployeeList';
+import ProductsList from './components/Products/ProductsList';
+import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/employees">Utilisateurs</Link>
+          </li>
+          <li>
+            <Link to="/products">Produits</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
 
 function App() {
   return (
-    <EmployeeList />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/employees" element={<EmployeeList />} />
+          <Route path="/products" element={<ProductsList />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
